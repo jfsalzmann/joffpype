754532921c49917a1b3ed34618dd797112e0333f
George Lewis <georgejeffreylewis@gmail.com>
2021-07-07 23:51:30 -0400

    current

--- tests/newtest.py
+++ tests/newtest.py
@@ -1,32 +1,39 @@
 
 from ast import Call, parse, Name, NodeTransformer, LShift, RShift, \
-    increment_lineno, walk, Attribute, BinOp, dump, List, Tuple, Starred
+    increment_lineno, walk, Attribute, BinOp, dump, List, Tuple, Starred, JoinedStr
 from inspect import getsource, isclass, stack
 from itertools import takewhile
 from textwrap import dedent
 
-PLACEHOLDER = '_'
+SUB_IDENT = '_'
 
-# def all_left(node):
+def check_name(item):
+    if isinstance(item, Name):
+        if item.id == SUB_IDENT:
+            return True
+    return False
 
+def check_starred(item):
+    if isinstance(item, Starred):
+        return check_name(item.value)
 
 
 class MyTransform(NodeTransformer):
-    def substitute(L, i, sub):
-        item = L[i]
-
-        if isinstance(item, Name):
-            if item.id == PLACEHOLDER:
-                L[i] = self.visit(sub)
-
-        if isinstance(item, Starred):
-            if isinstance(item.value, Name):
-                if item.value.id == PLACEHOLDER:
-                    item.value = self.visit(sub)
+    
+    def handle_tuple_list(self, L, replace):
+        if isinstance(L, (Tuple, List)):
+            for i, el in enumerate(L.elts):
+                if check_name(el):
+                    L.elts[i] = self.visit(replace)
+                    return True
+                if check_starred(el):
+                    L.elts[i].value = self.visit(replace)
+                    return True
+        return False
 
     def visit_BinOp(self, node):
-        print(dump(node, indent=4))
-        print('---')
+        # print(dump(node, indent=4))
+        # print('---')
         if isinstance(node.op, (LShift, RShift)):
             # if isinstance(node.right, Attribute):
             #     if isinstance(node.right.value, Name):
@@ -35,11 +42,11 @@ class MyTransform(NodeTransformer):
             #             return self.visit(node.right)
             if isinstance(node.right, BinOp):
                 if isinstance(node.right.left, Name):
-                    if node.right.left.id == PLACEHOLDER:
+                    if node.right.left.id == SUB_IDENT:
                         node.right.left = self.visit(node.left)
                         return node.right
                 if isinstance(node.right.right, Name):
-                    if node.right.right.id == PLACEHOLDER:
+                    if node.right.right.id == SUB_IDENT:
                         node.right.right = self.visit(node.left)
                         return node.right
             if isinstance(node.right, Call):
@@ -47,23 +54,54 @@ class MyTransform(NodeTransformer):
                 # _.func
                 if isinstance(node.right.func, Attribute):
                     if isinstance(node.right.func.value, Name):
-                        if node.right.func.value.id == PLACEHOLDER:
+                        if node.right.func.value.id == SUB_IDENT:
                             node.right.func.value = self.visit(node.left)
                             return node.right
 
+                once = False
+
                 # func(x, y, _, z)
                 for i, arg in enumerate(node.right.args):
-                    if isinstance(arg, Name):
-                        if arg.id == PLACEHOLDER:
-                            node.right.args[i] = self.visit(node.left)
-                            return node.right
+                    if check_name(arg):
+                        node.right.args[i] = self.visit(node.left)
+                        once = True
+                    
+                    if check_starred(arg):
+                        node.right.args[i].value = self.visit(node.left)
+                        once = True
 
+                if once:
+                    return node.right
+
+            # Lists and Tuples
             if isinstance(node.right, (List, Tuple)):
+                once = False
                 for i, el in enumerate(node.right.elts):
-                    if isinstance(el, Name):
-                        if el.id == PLACEHOLDER:
-                            node.right.elts[i] = self.visit(node.left)
-                            return node.right
+
+                    # 5 >> [x, y, _, z]
+                    if check_name(el):
+                        node.right.elts[i] = self.visit(node.left)
+                        once = True
+
+                    # (1, 2) >> [x, y, *_, z]
+                    if check_starred(el):
+                        node.right.elts[i].value = self.visit(node.left)
+                        once = True
+
+                    # if isinstance(el, BinOp):
+                    #     self.visit(el)
+
+                if once:
+                    return node.right
+
+            if isinstance(node.right, JoinedStr):
+                for i, fvalue in enumerate(node.right.values):
+                    if check_name(fvalue.value):
+                        node.right.values[i].value = self.visit(node.left)
+                        return node.right
+                    if check_starred(fvalue.value):
+                        node.right.values[i].value.value = self.visit(node.left)
+                        return node.right
 
             # Convert function name / lambda etc without braces into call
             if not isinstance(node.right, Call):
@@ -127,7 +165,7 @@ def pipes(func_or_class):
     # Apply the visit_BinOp transformation
     tree = MyTransform().visit(tree)
 
-    print(dump(tree))
+    # print(dump(tree))
 
     # now compile the AST into an altered function or class definition
     code = compile(
@@ -158,14 +196,53 @@ def pipes(func_or_class):
 # def bar():
 #     return 1 >> (_, 5)
 
+# @pipes
+# def ff():
+#     1 >> _+10 >> _*6 >> print(55, _, 77)
+
+# ff()
+
+# @pipes
+# def qq():
+#     [1, 2, 3] >> [1, *_] >> print(1, *_, 5)
+
+# qq()
+
+# @pipes
+# def tt():
+#     10 >> _**3>> [_] >> print
+
+# tt()
+
+# @pipes
+# def ee():
+#     10 >> [1, _, 100] >> f"the list: {_}" >> print
+
+# ee()
+
+# pylint: disable=undefined-variable
 @pipes
-def ff():
-    1 >> _+10 >> _*6 >> print(55, _, 77)
+def cc():
+    range(-5, 5) \
+    << map(lambda x: x + 1) \
+    << map(abs) \
+    >> [-10, *_] \
+    >> f"thing: {_}" \
+    >> print
 
-ff()
+# cc()
+
+square = lambda x: x * x
 
 @pipes
-def qq():
-    [1, 2, 3] >> [1, *_] >> print(1, _, 5)
+def pogchamp():
+    for x in range(10) << map(square):
+        print(x) # Alternatively: x >> print
 
-qq()
+pogchamp()
+
+@pipes
+def pp():
+    5 >> [_, _, 7, _ + 7] >> print
+
+pp()
