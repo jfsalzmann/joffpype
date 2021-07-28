106fb1e245a93e882757ab067b5f15c879b80b0f
George Lewis <georgejeffreylewis@gmail.com>
2021-07-27 23:48:15 -0400

    add new helpers to init

--- superpipe/__init__.py
+++ superpipe/__init__.py
@@ -2,4 +2,4 @@
 
 from .superpipe import pipes
 
-from .utils import foreach, is_none, is_not_none, is_even, is_odd, square, cube
+from .utils import foreach, is_none, is_not_none, is_even, is_odd, square, cube, is_falsy, is_truthy
